719398a5a340b5aa05896d3ecbd01be1597c9566
Ben Giamarino <giamarinob@gmail.com>
2016-09-20 01:07:47 +0000

    add stylesheet for index page, add middleware for public folder

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     app     = express();
 
+app.use('/css', express.static(__dirname + '/client/css'));
+app.use('/imgs', express.static(__dirname + '/public/imgs'));
+app.use('/docs', express.static(__dirname + '/public/docs'));
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/client/views/index.html');
 });
